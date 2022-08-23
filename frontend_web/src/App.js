0524362e7d650f711e5c4ea9ddca22a47db57f4b
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-08-23 21:12:53 +0530

    confirm

--- frontend_web/src/App.js
+++ frontend_web/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Header, Home, Contact, Footer } from './components';
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
+import "./index.css"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+    </main>
+  )
 }
 
-export default App;
+export default App
